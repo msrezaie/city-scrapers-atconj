f8d8b6cd11da67fb032b4023fdd7476000d7e8f4
msrezaie <79583632+msrezaie@users.noreply.github.com>
2024-12-13 13:40:12 -0500

    spider for Atlantic County Commissioners

---
+++ city_scrapers/spiders/atconj_County_Commission.py
@@ -0,0 +1,100 @@
+import json
+
+import scrapy
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.parser import parse as dateparse
+from scrapy import Selector
+
+
+class AtconjCountyCommissionSpider(CityScrapersSpider):
+    name = "atconj_County_Commission"
+    agency = "Atlantic County Board of County Commissioners"
+    timezone = "America/New_York"
+
+    original_url = "https://www.atlanticcountynj.gov"
+    meetings_url = "https://www.atlanticcountynj.gov/government/county-government/board-of-county-commissioners/meeting-schedule-agendas-and-minutes/-toggle-all"  # noqa
+
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+    }
+
+    default_location = {
+        "name": "Stillwater Building",
+        "address": "Stillwater Building, 201 S. Shore Road Northfield, New Jersey 08225",  # noqa
+    }
+
+    """
+    This website would return a 403 error if the request
+    is made with the default headers. The headers below
+    are the ones that are needed to make the request.
+    """
+
+    def start_requests(self):
+        headers = {
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36",  # noqa
+        }
+
+        yield scrapy.Request(
+            url=self.meetings_url, headers=headers, callback=self.parse
+        )
+
+    def parse(self, response):
+        rows = response.css("table.front_end_widget tbody tr").getall()
+        scripts = response.css("table.front_end_widget tbody script::text").getall()
+        time_note = response.css("div#widget_45_2122_758 p::text").get()
+
+        for row_item, script_item in zip(rows, scripts):
+            content = json.loads(script_item)
+
+            meeting = Meeting(
+                title="Atlantic County Board of County Commissioners",
+                description="",
+                classification=BOARD,
+                start=self._parse_start(content),
+                end=self._parse_end(content),
+                all_day=False,
+                time_notes=time_note.strip(),
+                location=self._parse_location(content),
+                links=self._parse_links(row_item),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        return dateparse(item["startDate"]).astimezone(tz=None).replace(tzinfo=None)
+
+    def _parse_end(self, item):
+        """Parse end datetime as a naive datetime object."""
+        return dateparse(item["endDate"]).astimezone(tz=None).replace(tzinfo=None)
+
+    def _parse_location(self, item):
+        location = item["location"]
+        if not location["address"] or not location["name"]:
+            return self.default_location
+        return {"name": location["name"], "address": location["address"]}
+
+    def _parse_links(self, item):
+        links = []
+
+        item = Selector(text=item)
+        agenda = item.css("td.event_agenda a").get()
+        minutes = item.css("td.event_minutes a").get()
+        if agenda:
+            agenda = agenda.split('href="')[1].split('"')[0]
+            links.append({"href": self.original_url + agenda, "title": "Agenda"})
+        if minutes:
+            minutes = minutes.split('href="')[1].split('"')[0]
+            links.append({"href": self.original_url + minutes, "title": "Minutes"})
+        return links
---
+++ tests/test_atconj_County_Commission.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.atconj_County_Commission import AtconjCountyCommissionSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "atconj_County_Commission.html"),
+    url="https://www.atlanticcountynj.gov/government/county-government/board-of-county-commissioners/meeting-schedule-agendas-and-minutes/-toggle-all",  # noqa
+)
+spider = AtconjCountyCommissionSpider()
+
+freezer = freeze_time("2024-12-13")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Atlantic County Board of County Commissioners"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2025, 1, 7, 16, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] == datetime(2025, 1, 7, 23, 59)
+
+
+def test_time_notes():
+    assert (
+        parsed_items[0]["time_notes"]
+        == "All Commissioner meetings held on First and Third Tuesdays at 4:00 pm unless otherwise noted."  # noqa
+    )
+
+
+def test_id():
+    assert (
+        parsed_items[0]["id"]
+        == "atconj_County_Commission/202501071600/x/atlantic_county_board_of_county_commissioners"  # noqa
+    )
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "tentative"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "Atlantic County Institute of Technology",
+        "address": "Atlantic County Institute of Technology, 5080 Atlantic Avenue Mays Landing, New Jersey 08330",  # noqa
+    }
+
+
+def test_source():
+    assert (
+        parsed_items[0]["source"]
+        == "https://www.atlanticcountynj.gov/government/county-government/board-of-county-commissioners/meeting-schedule-agendas-and-minutes/-toggle-all"  # noqa
+    )
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
